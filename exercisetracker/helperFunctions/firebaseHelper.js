f869c8f35fd08a5ac4431fc05269dc256eeeb57d
shareysanr <shareysanr1@gmail.com>
2022-08-21 02:20:41 -0400

    Adding stats to firebaseHelper

--- exercisetracker/helperFunctions/firebaseHelper.js
+++ exercisetracker/helperFunctions/firebaseHelper.js
@@ -15,65 +15,11 @@ import {
 
 import { encode, decode } from "firebase-encode";
 
-export function addAge(auth, num) {
+export function addStats(auth, obj) {
   let userId = auth.user.uid;
   let colRef = collection(database, userId);
   let docRef = doc(colRef, "stats");
 
-  setDoc(docRef, { age: num}, { merge: true});
-  return;
-}
-
-export function addGender(auth, str) {
-  let userId = auth.user.uid;
-  let colRef = collection(database, userId);
-  let docRef = doc(colRef, "stats");
-
-  setDoc(docRef, { gender: str}, { merge: true});
-  return;
-}
-
-export function addWeight(auth, num) {
-  let userId = auth.user.uid;
-  let colRef = collection(database, userId);
-  let docRef = doc(colRef, "stats");
-
-  setDoc(docRef, { weight: num}, { merge: true});
-  return;
-}
-
-export function addHeight(auth, num) {
-  let userId = auth.user.uid;
-  let colRef = collection(database, userId);
-  let docRef = doc(colRef, "stats");
-
-  setDoc(docRef, { height: num}, { merge: true});
-  return;
-}
-
-export function addNeck(auth, num) {
-  let userId = auth.user.uid;
-  let colRef = collection(database, userId);
-  let docRef = doc(colRef, "stats");
-
-  setDoc(docRef, { neck: num}, { merge: true});
-  return;
-}
-
-export function addWaist(auth, num) {
-  let userId = auth.user.uid;
-  let colRef = collection(database, userId);
-  let docRef = doc(colRef, "stats");
-
-  setDoc(docRef, { waist: num}, { merge: true});
-  return;
-}
-
-export function addHip(auth, num) {
-  let userId = auth.user.uid;
-  let colRef = collection(database, userId);
-  let docRef = doc(colRef, "stats");
-
-  setDoc(docRef, { hip: num}, { merge: true});
+  setDoc(docRef, { age: obj.age, gender: obj.gender, weight: obj.weight, height: obj.height, neck: obj.neck, waist: obj.waist, hip: obj.hip}, { merge: true});
   return;
 }
